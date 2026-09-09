232d224b7183249cd31f7d01443774a4911d6299
Xelovnight <153280657+Xelovnight@users.noreply.github.com>
2026-09-09 16:44:14 +0200

    modification deploy

--- mon-portfolio/astro.config.mjs
+++ mon-portfolio/astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://xelovnight.github.io',
-  base: '/Portfolio/',
+  base: '/Portfolio/mon-portfolio/',
   vite: {
     plugins: [tailwindcss()]
   },
